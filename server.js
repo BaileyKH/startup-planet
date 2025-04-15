2004f1af508a22ecb0e4f0ffe59452de0e294c8b
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2025-04-15 09:39:42 -0700

    Update: Modularized routes and controllers

--- server.js
+++ server.js
@@ -1,46 +1,13 @@
 import express from 'express'
-import { startups } from './data/data'
+import { apiRouter } from './routes/apiRoutes.js'
 
 const PORT = 8000
 
 const app = express()
 
-app.get('/api', (req, res) => {
-    let filteredData = startups
-
-    const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
-  
-    if (industry) {
-      filteredData = filteredData.filter( startup => 
-        startup.industry.toLowerCase() === industry.toLowerCase()
-      )
-    }
-  
-    if (country) {
-      filteredData = filteredData.filter( startup => 
-        startup.country.toLowerCase() === country.toLowerCase()
-      )
-    }
-    
-    if (continent) {
-      filteredData = filteredData.filter( startup => 
-        startup.continent.toLowerCase() === continent.toLowerCase()
-      )
-    }
-  
-    if (is_seeking_funding) {
-      filteredData = filteredData.filter( startup => 
-        startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
-      )
-    }
-    if (has_mvp) {
-      filteredData = filteredData.filter( startup => 
-        startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
-      )
-    }
-  
-  
-    res.json(filteredData)
+app.use('/api', apiRouter)
+app.use((req, res) => {
+    res.status(404).json({ message: "Endpoint not found. Please check the API documentation." })
 })
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`))
+app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
